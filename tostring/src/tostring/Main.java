86a1a5a5e0d8ca887b75d0c0c662a2b1b51adce5
user <user@user-ThinkPad-T440s>
2021-08-07 06:17:53 -0400

    commit

---
+++ tostring/src/tostring/Main.java
@@ -0,0 +1,15 @@
+package tostring;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car = new Car();
+		
+		
+		System.out.println(car.toString());
+		System.out.println(car);
+
+	}
+
+}
